b3023ee828b98cb447d82631768a9e9fd17ea661
Matt C <matt@artemisbot.uk>
2018-09-05 16:05:00 +0100

    Spooky commit with scary PWAs
    aaaa

---
+++ CyberChef/service-worker.js
@@ -0,0 +1,30 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("dist/workbox-v3.4.1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "dist/workbox-v3.4.1"});
+
+importScripts(
+  "dist/precache-manifest.34eb1af0fee621a8918874d471fd79bf.js"
+);
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/\//, workbox.strategies.networkFirst({ networkTimeoutSeconds: 10, plugins: [new workbox.cacheableResponse.Plugin({"statuses":[0,200]})] }), 'GET');
